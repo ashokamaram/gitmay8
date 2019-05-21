5df34ad5312eac5c1a7919c8e28b0b7ee1af8a1f
root <root@ip-10-10-1-151.ec2.internal>
2019-05-21 13:13:20 +0000

    added echo file creation

--- usercreate.sh
+++ usercreate.sh
@@ -4,3 +4,5 @@ read -p "please enter the user name:" USERNAME
 echo $USERNAME
 read -s -p "please enter password:" PASSWORD
 useradd -m $USERNAME -P $PASSWORD
+echo "user $USERNAME is created sucesssfully...!!!!"
+
